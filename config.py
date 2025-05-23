69e6b2023d7b0be45033e8993d0cf88bd9f56126
RezaDargahi2003 <rezadargahi2003@gmail.com>
2025-05-23 19:09:03 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+TELEGRAM_TOKEN = "8103030808:AAHSXYKjylf1RVLGEtZsMeDmx4E5ZNUTe38"
+ADMIN_ID = 648261997  # آیدی عددی شما
+
+PRICING = {
+    "monthly": {"amount": 50, "days": 30},
+    "6months": {"amount": 200, "days": 180},
+    "yearly": {"amount": 300, "days": 365}
+}
+
+WELCOME_MESSAGE = """
+به ربات *رادار نهنگ‌ها* خوش آمدید!
+
+با استفاده از این ربات می‌توانید سیگنال‌های دقیق بازار رمزارز را دریافت کنید.
+
+از منوی ربات یکی از گزینه‌ها را انتخاب کنید.
+"""
+
+COINMARKETCAP_API_KEY = "کلید API مربوط به CoinMarketCap"
